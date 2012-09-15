a2d40149a9b53a63a800bd84b73ca3f212315742
Kopakc <kopakc.true@gmail.com>
2012-09-16 02:07:53 +0400

    More correct surface normal from heightmap calculation

--- glsl/SurfaceVertexShader.glsl
+++ glsl/SurfaceVertexShader.glsl
@@ -18,12 +18,21 @@ void main()
 	float y = h * (boxMax.y - boxMin.y) + boxMin.y;
 	vec4 inView = gl_ModelViewMatrix * vec4(x, y, z, 1);
 	gl_Position = gl_ProjectionMatrix * inView;
-	float hx = texture2D(heightMap, vec2(s+.01,t)).x;
-	float hz = texture2D(heightMap, vec2(s,t+.01)).x;
-	vec3 N = cross(vec3(0,hz-h,.01), vec3(.01,hx-h,0));
-	vec4 matColor = y < 200 ? vec4(0,.6,0,1) : vec4(.6,.6,.6,1);
+	
+	
+	float ds = .01, dt = .01;//TODO: calculate these to get plus/minus 1 texel
+	float dx = ds * (boxMax.x - boxMin.x) / (boxMax.y - boxMin.y);
+	float dz = dt * (boxMax.z - boxMin.z) / (boxMax.y - boxMin.y);
+	float hx1 = texture2D(heightMap, vec2(s-ds,t)).x;
+	float hx2 = texture2D(heightMap, vec2(s+ds,t)).x;
+	float hz1 = texture2D(heightMap, vec2(s,t-dt)).x;
+	float hz2 = texture2D(heightMap, vec2(s,t+dt)).x;
+	vec3 N = cross(vec3(0,hz2-hz1,2*dz), vec3(2*dx,hx2-hx1,0));
 	vec3 vNor = gl_NormalMatrix * N;
-	float f = .5 - .5 * dot(normalize(vLightDir), normalize(vNor));
+	
+	
+	vec4 matColor = y < 300 ? vec4(0,.6,0,1) : vec4(.6,.6,.6,1);
+	float f = .4 - .4 * dot(normalize(vLightDir), normalize(vNor));
 	myColor = matColor * f;
 	fogFactor = clamp(30000.f / length(inView), 0, 1);
 }
